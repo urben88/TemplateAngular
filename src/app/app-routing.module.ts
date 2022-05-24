9949b83f0083320ac751b19867c4a48cb40ea715
Ruben Esteve <resteve@vidal-ac.es>
2022-05-24 09:15:22 +0200

    Paso proyecto template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  //? Ruta para la página principal
+  {path:'',
+    loadChildren: () => import('./core/core.module').then( m => m.CoreModule)
+  },
+  
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{
+    scrollPositionRestoration:'enabled',
+    //? Este parámetro es para evitar el error de rutas al desplegar la aplicación angular
+    useHash:true
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
