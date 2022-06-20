1a35558c6a9201374f787d23435d46c6b9e859e6
Urben <tirolin25@gmail.com>
2022-06-20 16:13:21 +0200

    Añado carpetas

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreComponent } from './core.component';
+import { CoreRoutingModule } from './core-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    CoreComponent
+    CoreComponent,
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreRoutingModule
   ]
 })
 export class CoreModule { }
